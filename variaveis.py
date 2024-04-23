14442e7858c7903e8a3587e7f50fdd5b2dfd697d
AnaCarolineSilvaLima <anacarolinelima1212@gmail.com>
2024-04-23 16:43:31 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Ana Caroline"
+idade=16
+email="carolanasilva2007@gmail.com"
+telefone="17 20305-3060"
+nota=7.99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
