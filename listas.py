44fe72e24f199ce5df2842e742db23dd5f4766ad
AnaCarolineSilvaLima <anacarolinelima1212@gmail.com>
2024-05-21 14:59:53 -0300

    aprovado/reprovado

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Ana Caroline", 10, 4.8, False]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Média: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Não")
