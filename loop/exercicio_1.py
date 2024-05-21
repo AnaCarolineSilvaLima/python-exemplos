410bd32c7fbf2703fa7a41bf16a850c6b6b44712
AnaCarolineSilvaLima <anacarolinelima1212@gmail.com>
2024-05-21 14:33:34 -0300

    delete loop

--- loop/exercicio_1.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Digite quantos números você quer calcular na média: "))
-soma = 0
-iteracao = 1
-while iteracao<=n:
-    numero = float(input("Digite um número: "))
-    soma += numero
-    iteracao += 1
-print(f"A média é igual a {soma/n}")    
